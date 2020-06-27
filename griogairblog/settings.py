bd6d8b4863d69adf53fb5c177f9369c8fac8318f
Shinny <sineadward@protonmail.com>
2020-06-27 15:17:32 +0200

    minor edits

--- griogairblog/settings.py
+++ griogairblog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=1b=^9)udis_p82u+f)qq*u%v+hk*bc9o)b=y*$@2yr3zmgin^'
+# SECRET_KEY = '=1b=^9)udis_p82u+f)qq*u%v+hk*bc9o)b=y*$@2yr3zmgin^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
